92ea13ef4942725db1aaa1ce2792db87c4dbe73f
Alex <alexander.g@btigroup.io>
2019-04-28 23:37:57 +0300

    Sample with settings per environment using generic host

---
+++ ConsoleSettingsAndService/src/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CoreSample
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await new HostBuilder()
+                .ConfigureHostConfiguration(config =>
+                {
+                    if (args != null)
+                    {
+                        // enviroment from command line
+                        // e.g.: dotnet run --environment "Staging"
+                        config.AddCommandLine(args);
+                    }
+                })
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    var environment = context.HostingEnvironment;
+                    builder.SetBasePath(AppContext.BaseDirectory)
+                        .AddJsonFile("appsettings.json", optional: false)
+                        .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<SampleService>();
+                })
+                .RunConsoleAsync(CancellationToken.None);
+        }
+    }
+}
---
+++ ConsoleSettingsAndService/src/SampleService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace CoreSample
+{
+    public class SampleService : IHostedService
+    {
+        private readonly IConfiguration _configuration;
+
+        public SampleService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("MainSetting: " + _configuration["MainSetting"]);
+            Console.WriteLine("AppSettings - Value: " + _configuration.GetSection("AppSettings")["Value"]);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
